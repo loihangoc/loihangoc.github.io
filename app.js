6cc7f7407db620d9026673f82032d35fc637ffe7
Loi Ha <loihangoc@gmail.com>
2023-06-17 23:22:55 -0400

    Add image carousel

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 let images = [...document.querySelectorAll('.img')];
-let slider = document.getElementsByClassName('slider');
+let slider = document.querySelectorAll('.slider');
 let sliderWidth;
-let imageWidth = 0;
+let imageWidth;
 let current = 0;
 let target = 0;
-let ease = 0.5
+let ease = 0.5;
+
+window, addEventListener('resize', init);
 
 images.forEach((img, idx) => {
-  img.style.backGroundImage = `url(../images/${idx+1}.jpg)`
+  img.style.backgroundImage = `url(./images/${idx+1}.jpg)`;
 })
 
 function lerp(start, end, t){
@@ -19,9 +21,32 @@ function setTransform(el, transform){
 }
 
 function init(){
-  sliderWidth = slider.getBoundingClientRect().width;
+  sliderWidth = slider[0].getBoundingClientRect().width;
   imageWidth = sliderWidth / images.length;
-  document.body.style.height = `${sliderWidth - (window.innerWidth - window.innerHeight)}px`
+  document.body.style.height = `${sliderWidth - (window.innerWidth - window.innerHeight)}px`;
+}
+
+function animate(){
+  current = parseFloat(lerp(current, target, ease)).toFixed(2);
+  target = window.scrollY;
+  setTransform(slider[0], `translateX(-${current}px`)
+  animateImages();
+  requestAnimationFrame(animate);
+}
+
+function animateImages(){
+  let ratio = current / imageWidth;
+  let intersectionRatioValue;
+
+  images.forEach((image, idx) => {
+    intersectionRatioValue = ratio - (idx * 0.7);
+    setTransform(image, `translateX(${intersectionRatioValue * 70}px)`)
+  })
 }
 
 init();
+animate();
+
+console.log(sliderWidth);
+
+console.log(imageWidth);
